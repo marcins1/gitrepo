903f8c4d50d7685cb528098cf79a9fcabe108b1e
marcins1 <rondusiowy@gmail.com>
2018-05-11 13:30:45 +0200

    Operacje dodawania i aktualizacji sqlite3

--- SQL/szkola.sql
+++ SQL/szkola.sql
@@ -21,3 +21,12 @@ CREATE TABLE tbKlasa
 	rokNaboru INTEGER,
 	rokMatury INTEGER
 );
+
+INSERT INTO tbKlasa(id, klasa, rokNaboru, rokMatury) VALUES (NULL, '1A', 2017, 2020);
+INSERT INTO tbKlasa VALUES (NULL, '2A', 2016, 2019);
+INSERT INTO tbKlasa VALUES (NULL, '1C', 2017, 2020);
+INSERT INTO tbUczniowie(id, imie, nazwisko, plec, id_klasa, egzHum, egzMat, egzJez)
+VALUES (NULL, 'Adam', 'Słodowy', 0, 3, 70.5, 80, 90);
+INSERT INTO tbUczniowie VALUES (NULL, 'Marcin', 'Saja', 0, 1, 73, 93, 83);
+
+UPDATE tbUczniowie SET egzJez = 100 WHERE id = 1;
