181490f8c603fce544b93d5acc59d288a7883a46
marcins1 <rondusiowy@gmail.com>
2018-10-23 19:26:58 +0200

    Dokonczenie programu liczacego nwd i nww w c++

--- cpp/euklides.cpp
+++ cpp/euklides.cpp
@@ -7,14 +7,28 @@
 
 using namespace std;
 
-// nwd1()
+int nwdl(int a, int b){
+    while(a!=b){
+        if(a > b){
+            a -= b;
+        } else {
+            b -= a;
+        }
+    }
+    return a;
+}
+
+int nwwl(int a, int b){
+    return a*b/nwdl(a, b);
+}
 
 int main(int argc, char **argv){
     int a, b;
     cout << "Podaj 2 liczby: ";
     cin >> a >> b;
-    int nwd = nwd1(a, b);
-    cout << "NWD(" << a << "," << b << " ) = " << nwd << endl;
+    int wynik = nwdl(a, b);
+    cout << "NWD(" << a << "," << b << ") = " << wynik << endl;
+    wynik = nwwl(a, b);
+    cout << "NWW(" << a << "," << b << ") = " << wynik << endl;
 	return 0;
 }
-
