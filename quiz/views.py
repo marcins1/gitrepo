7eb9c9771fc4a7f698ef63288c9da1d4e6ca5433
marcins1 <rondusiowy@gmail.com>
2019-01-28 08:46:03 +0100

    Rozwijanie aplikacji quiz

--- quiz/views.py
+++ quiz/views.py
@@ -7,6 +7,7 @@
 from flask import Flask
 from flask import render_template, request, flash, redirect, url_for
 from modele import Kategoria, Pytanie, Odpowiedz
+from forms import *
 
 app = Flask(__name__)
 
@@ -34,6 +35,12 @@ def quiz():
     
     pytania = Pytanie.select().join(Odpowiedz).distinct()
     return render_template('quiz.html', pytania=pytania)
+
+@app.route("/dodaj", methods=['GET', 'POST'])
+def dodaj():
+    """Dodawanie pytań i odpowiedzi"""
+    form = DodajForm()
+    return render_template('dodaj.html', form=form)
 
 def main(args):
     return 0
