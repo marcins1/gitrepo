738c0c98acf155111f480acd43ae560b610301b7
marcins1 <rondusiowy@gmail.com>
2019-01-31 07:56:40 +0100

    Systemy liczbowe

---
+++ cpp/liczbydodatnie.cpp
@@ -0,0 +1,21 @@
+/*
+ * liczbydodatnie.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int n, i;
+    cin >> n;
+    i = 1;
+    while(i!=n){
+        cout << i << " ";
+        i+=2;
+    }
+	return 0;
+}
+
